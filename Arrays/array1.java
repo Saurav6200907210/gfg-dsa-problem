ed3e205d082b982f172d9191972e7d0064aa6b2c
Saurav6200907210 <sonukumarteg245@gmail.com>
2025-08-20 00:49:22 +0530

    array1

---
+++ Arrays/array1.java
@@ -0,0 +1,18 @@
+class Solution {
+    public Pair<Integer, Integer> getMinMax(int[] arr) {
+        // Code Here
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        for(int i=0;i<arr.length;i++){
+            
+            if(arr[i]<min){
+                min=arr[i];
+            }
+            if(arr[i]>max){
+                max=arr[i];
+            }
+           
+        }
+        return new Pair(min, max);
+    }
+}
